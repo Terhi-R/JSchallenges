fbf5795a133f0bde61fa1f8ac0a2944d181f354f
Terhi-R <112477395+Terhi-R@users.noreply.github.com>
2022-10-30 17:25:30 +0100

    back

--- JavascriptChallenges.js
+++ JavascriptChallenges.js
@@ -1,7 +1,6 @@
 let secondHand = document.querySelector(".secHand");
 let minuteHand = document.querySelector(".minHand");
 let hourHand = document.querySelector(".hourHand");
-let clockRound = document.querySelector(".clockRound");
 
 function setDate() {
   let now = new Date();
@@ -14,9 +13,6 @@ function setDate() {
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-  if (hourHand) {
-    clockRound.style.padding = `4rem`;
-  }
 }
 
 setInterval(setDate, 1000);
